f03716caddca6b38ad74f2292523335d28e769db
unknown <gildemardiniz@gmail.com>
2025-03-16 17:03:59 -0300

    algumas modificacoesx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import FmdGoodIcon from '@mui/icons-material/FmdGood';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import './Constantes.css';
 import perfil from './assets/perfil.jpg';
 
@@ -22,12 +22,18 @@ function App() {
         </div>
         <h4 className="d-flex justify-content-center mt-3 text-center">Comunidade</h4>
         <h4 className="d-flex justify-content-center mb-3 text-center">Nossa Senhora das Graças</h4>
-        <Outlet/>
+        <Outlet />
         <footer className='mt-5 mb-4'>
           <h6 className="d-flex justify-content-center m-3 text-center ">Paróquia de Santo Antônio - Parque Vitória</h6>
           <div className="d-flex justify-content-center">
             <a className='text-reset text-color-azul text-decoration-none' href='https://www.instagram.com/paroquia_santoantoniopv'><InstagramIcon className='mx-1 color-azul' /></a>
             <a className='text-reset text-color-azul text-decoration-none' href='https://maps.app.goo.gl/9oyHksR1tHbYLzmW9'><FmdGoodIcon className='mx-1 color-azul' /></a>
+          </div>
+
+          <div className="col-12 my-3 text-center mt-4">
+            <h6>Copyright &copy; {new Date().getFullYear()}. Todos os direitos reservados | Desenvolvido por
+              <Link className='text-decoration-none text-color-roxo' to='https://dinizdev.vercel.app/'> _dinizDev</Link>
+            </h6>
           </div>
 
         </footer>
--- src/routes/Links.jsx
+++ src/routes/Links.jsx
@@ -5,29 +5,42 @@ export const Links = () => {
     return (
 
         <div>
-            <div className="d-flex justify-content-center">
-                <button type="button" className="btn btn-light m-2 col-12 col-md-6 p-3 rounded-pill border-2 color-borda">
-                    <a className='text-reset text-color-azul text-decoration-none' href='https://www.instagram.com/nsadasgracas_jardimturu'>Instagram</a>
-                </button>
-            </div>
-
-            <div className="d-flex justify-content-center">
-                <button type="button" className="btn btn-light m-2 col-12 col-md-6 p-3 rounded-pill border-2 color-borda">
-                    <a className='text-reset text-color-azul text-decoration-none' href='https://wa.me/559885799266'>Whatsapp</a>
-                </button>
-            </div>
-
-            <div className="d-flex justify-content-center">
-                <button type="button" className="btn btn-light m-2 col-12 col-md-6 p-3 rounded-pill border-2 color-borda">
-                    <a className='text-reset text-color-azul text-decoration-none' href='https://maps.app.goo.gl/YYVyAAGKoRDzQfc98'>Localização</a>
-                </button>
-            </div>
-            <div className="d-flex justify-content-center">
-                <button type="button" className="btn btn-light m-2 col-12 col-md-6 p-3 rounded-pill border-2 color-borda">
-                    <Link className='text-reset text-color-azul text-decoration-none' to="/pix">Dizimo & doações</Link>
-                </button>
-            </div>
-        </div>
+            <Link to='https://maps.app.goo.gl/YYVyAAGKoRDzQfc98' className="text-decoration-none">
+                <div className="d-flex justify-content-center">
+                    <button type="button" className="btn btn-light m-2 col-12 col-md-6 p-3 rounded-pill border-2 color-borda">
+                        Localização
+                    </button>
+                </div>
+            </Link>
+            <Link to='/pix' className="text-decoration-none">
+                <div className="d-flex justify-content-center">
+                    <button type="button" className="btn btn-light m-2 col-12 col-md-6 p-3 rounded-pill border-2 color-borda">
+                        Dizimo & doações
+                    </button>
+                </div>
+            </Link>
+            <Link to='https://wa.me/559887506183' className="text-decoration-none">
+                <div className="d-flex justify-content-center">
+                    <button type="button" className="btn btn-light m-2 col-12 col-md-6 p-3 rounded-pill border-2 color-borda">
+                        Whatsapp
+                    </button>
+                </div>
+            </Link>
+            <Link to='https://chat.whatsapp.com/A8QfF5HGWSK7KhbjUkXEo6%2016:48' className="text-decoration-none">
+                <div className="d-flex justify-content-center">
+                    <button className="btn btn-light m-2 col-12 col-md-6 p-3 rounded-pill border-2 color-borda ">
+                        Entre no Grupo
+                    </button>
+                </div>
+            </Link>
+            <Link to='https://www.instagram.com/nsadasgracas_jardimturu' className="text-decoration-none">
+                <div className="d-flex justify-content-center">
+                    <button className="btn btn-light m-2 col-12 col-md-6 p-3 rounded-pill border-2 color-borda">
+                        Instagram
+                    </button>
+                </div>
+            </Link>
+        </div >
 
     )
 
